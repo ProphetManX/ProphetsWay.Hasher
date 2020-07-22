3bd0cdb84a92fed49cfe9928fa5202ea44c1de43
G. Gordon Nasseri <ProphetManX@gmail.com>
2020-07-22 15:14:15 -0400

    created a test project, and added three test files for use in verifying the hasher logic is correct.

---
+++ ProphetsWay.Hasher.Tests/HasherTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ProphetsWay.Hasher.Tests
+{
+	public class HasherTests
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
---
+++ ProphetsWay.Hasher.Tests/TestFiles.cs
@@ -0,0 +1,32 @@
+﻿namespace ProphetsWay.Hasher.Tests
+{
+	internal static class TestFiles
+	{
+		public static class TestFileA
+		{
+			public const string Name = "TestFileA.txt";
+			public const string MD5 = "94368a509401d8e56ea5e8c1592262ff";
+			public const string SHA1 = "5943684f0a4af2f0e318ef5ea1803d26b521214a";
+			public const string SHA256 = "4b20b968d857a92ddc033d1b374cc944b978db96cb7d9056e60bc9f31f3fba14";
+			public const string SHA512 = "bd0f33984375e47737098050bfea5468b76675942e1bd22ce6744590ed4bfe6780e02a632fa98f310a004931d19f2627fd7ddc62c0609ca320409110ed273874";
+		}
+
+		public static class TestFileB
+		{
+			public const string Name = "TestFileB.bmp";
+			public const string MD5 = "7f5f5b7b76e6cef0e1271c421a9d2e1a";
+			public const string SHA1 = "253eecc784d7249fb3b61448f80054bd886c16e0";
+			public const string SHA256 = "03378b1990ca7197d925ba6c9f77a1636fa4b590b7a32f1278f6449d17bceb35";
+			public const string SHA512 = "f0e5f8c33f3dd29e94d83141cd7912b30377cd76f5fbbf516a03382be0daf6b6dc2d851ef73b0039e7178bbb1d9f3f926ec16bd0171ce57a69f6d17665ab6b0e";
+		}
+
+		public static class TestFileC
+		{
+			public const string Name = "TestFileC.bin";
+			public const string MD5 = "f4d61fcb46717d50e9bac2dbaf274cc9";
+			public const string SHA1 = "60982813bf73f2acf8b4d8603021547299d7b03d";
+			public const string SHA256 = "6b908d70ee1ac865a96f8fbee3c7b9f960d357cd5c02a43dac082f498c99514b";
+			public const string SHA512 = "cf60b1c0dad7207bce20e14e3177e57561732eb702892c30a7fec4fbb4ba76cfffd78fd636971aa4f299b81d15ac8ccb3f320506eaefb96b96f462dd09994f72";
+		}
+	}
+}
