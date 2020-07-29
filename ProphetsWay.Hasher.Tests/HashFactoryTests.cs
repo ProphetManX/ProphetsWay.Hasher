96440f0a5d21b9e9eefd6fe096d11eb0623f4732
G. Gordon Nasseri <ProphetManX@gmail.com>
2020-07-29 18:45:40 -0400

    #2 updated the enum as a flag, and assigned binary integers to support passing all the values together
    added a new GetHashers method to the factory, so you can retrieve a dictionary of HashAlgorithms, so you know what algorithm you have
    
    Created new unit tests for the HashFactory

---
+++ ProphetsWay.Hasher.Tests/HashFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using ProphetsWay.Utilities;
+using FluentAssertions;
+using System.Security.Cryptography;
+using System;
+
+namespace ProphetsWay.Hasher.Tests
+{
+	public class HashFactoryTests
+	{
+		[Theory]
+		[InlineData(HashTypes.MD5, typeof(MD5CryptoServiceProvider))]
+		[InlineData(HashTypes.SHA1, typeof(SHA1Managed))]
+		[InlineData(HashTypes.SHA256, typeof(SHA256Managed))]
+		[InlineData(HashTypes.SHA384, typeof(SHA384Managed))]
+		[InlineData(HashTypes.SHA512, typeof(SHA512Managed))]
+#if NET451 || NET452 || NET46 || NET461 || NET471 || NET472 || NET48
+		[InlineData(HashTypes.RIPEMD160, typeof(RIPEMD160Managed))]
+#endif
+		public void TestGetHasher(HashTypes hashType, Type type)
+		{
+			var hasher = hashType.GetHasher();
+			hasher.Should().BeOfType(type);
+		}
+
+		[Theory]
+		[InlineData(HashTypes.MD5, 1)]
+		[InlineData(HashTypes.MD5 | HashTypes.SHA1, 2)]
+		[InlineData(HashTypes.MD5 | HashTypes.SHA1 | HashTypes.SHA256, 3)]
+		[InlineData(HashTypes.MD5 | HashTypes.SHA1 | HashTypes.SHA256 | HashTypes.SHA384, 4)]
+		[InlineData(HashTypes.MD5 | HashTypes.SHA1 | HashTypes.SHA256 | HashTypes.SHA384 | HashTypes.SHA512, 5)]
+#if NET451 || NET452 || NET46 || NET461 || NET471 || NET472 || NET48
+		[InlineData(HashTypes.MD5 | HashTypes.SHA1 | HashTypes.SHA256 | HashTypes.SHA384 | HashTypes.SHA512 | HashTypes.RIPEMD160, 6)]
+#endif
+		public void TestGetHashers(HashTypes hashTypes, int numberOfHashers)
+		{
+			var hashers = hashTypes.GetHashers();
+			hashers.Count.Should().Be(numberOfHashers);
+		}
+	}
+}
+
