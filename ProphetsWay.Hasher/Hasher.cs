d31b0ebcdf9fc7e173ccb16da6e4493f88893c28
G. Gordon Nasseri <ProphetManX@gmail.com>
2020-07-29 19:18:06 -0400

    updated hasher to support the multi/flag enum option, updated the current "GenerateHashes" method to utilize the new functionality.

--- ProphetsWay.Hasher/Hasher.cs
+++ ProphetsWay.Hasher/Hasher.cs
@@ -9,12 +9,16 @@ namespace ProphetsWay.Utilities
 	{
 		private const int BUFFER_SIZE = 1024*1024*128;
 
+		internal static string ToLowerString(this byte[] bytes)
+		{
+			return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+		}
+
 		public static string GenerateHash(this Stream stream, HashTypes hashType)
 		{
 			var worker = new HashWorker(hashType);
 			worker.GenerateHash(stream);
-
-			return BitConverter.ToString(worker.Hash).Replace("-", "").ToLower();
+			return worker.Hash.ToLowerString();
 		}
 
 		public static bool VerifyHash(this Stream stream, string hash, HashTypes hashType)
@@ -37,6 +41,7 @@ namespace ProphetsWay.Utilities
 			return GenerateHash(fileInfo.OpenRead(), hashType);
 		}
 
+		[Obsolete("This method is deprecated.  Switch to using GenerateHashes(Stream, HashTypes) instead.")]
 		public static HashCollection GenerateHashes(this Stream stream)
 		{
 			var task = Task.Run(() => { return GenerateHashesAsync(stream); });
@@ -44,14 +49,17 @@ namespace ProphetsWay.Utilities
 			return task.Result;
 		}
 
-		public async static Task<HashCollection> GenerateHashesAsync(this Stream stream)
+		public static Dictionary<HashTypes, string> GenerateHashes(this Stream stream, HashTypes hashtypes)
+		{
+			var task = Task.Run(() => { return GenerateHashesAsync(stream, hashtypes); });
+			task.Wait();
+			return task.Result;	
+		}
+
+		private async static Task<Dictionary<HashTypes, byte[]>> GenerateHashesAsBytes(Stream stream, HashTypes hashtypes)
 		{
 			var buffer = new byte[BUFFER_SIZE];
-			var md5Worker = new HashWorker(HashTypes.MD5);
-			var sha1Worker = new HashWorker(HashTypes.SHA1);
-			var sha256Worker = new HashWorker(HashTypes.SHA256);
-			var sha512Worker = new HashWorker(HashTypes.SHA512);
-
+			var hashers = hashtypes.GetHashers();
 
 			await Task.Run(() =>
 			{
@@ -60,20 +68,41 @@ namespace ProphetsWay.Utilities
 				{
 					bRead = stream.Read(buffer, 0, buffer.Length);
 
-					var tasks = new List<Task>
-					{
-						Task.Run(() => md5Worker.GenerateIncrementalHash(buffer, bRead)),
-						Task.Run(() => sha1Worker.GenerateIncrementalHash(buffer, bRead)),
-						Task.Run(() => sha256Worker.GenerateIncrementalHash(buffer, bRead)),
-						Task.Run(() => sha512Worker.GenerateIncrementalHash(buffer, bRead))
-					};
+					var tasks = new List<Task>();
+
+					foreach (var hasher in hashers)
+						tasks.Add(Task.Run(() => hasher.Value.GenerateIncrementalHash(buffer, bRead)));
 
 					Task.WaitAll(tasks.ToArray());
 				} while (bRead > 0);
 			});
+			
+			var rslt = new Dictionary<HashTypes, byte[]>();
+			foreach (var hasher in hashers)
+				rslt.Add(hasher.Key, hasher.Value.Hash);
 
+			return rslt;
+		}
 
-			var ret = new HashCollection(md5Worker.Hash, sha1Worker.Hash, sha256Worker.Hash, sha512Worker.Hash);
+		public async static Task<Dictionary<HashTypes, string>> GenerateHashesAsync(this Stream stream, HashTypes hashtypes)
+		{
+			var byteResults = await GenerateHashesAsBytes(stream, hashtypes);
+
+			var rslt = new Dictionary<HashTypes, string>();
+			foreach (var br in byteResults)
+				rslt.Add(br.Key, br.Value.ToLowerString());
+
+			return rslt;
+		}
+
+		[Obsolete("This method is deprecated.  Switch to using GenerateHashesAsync(Stream, HashTypes) instead.")]
+		public async static Task<HashCollection> GenerateHashesAsync(this Stream stream)
+		{
+			var hashes = HashTypes.MD5 | HashTypes.SHA1 | HashTypes.SHA256 | HashTypes.SHA512;
+
+			var results = await GenerateHashesAsBytes(stream, hashes);
+
+			var ret = new HashCollection(results[HashTypes.MD5], results[HashTypes.SHA1], results[HashTypes.SHA256], results[HashTypes.SHA512]);
 			return ret;
 		}
 
